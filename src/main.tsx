34ef01bb788cb6712cab94ca0498c11d7e4323c8
KalebGore28 <91081805+KalebGore28@users.noreply.github.com>
2025-05-09 09:22:35 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,34 +4,39 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { RelayEnvironmentProvider } from "react-relay";
-import { Environment, Network, Store, RecordSource, FetchFunction } from "relay-runtime";
-
+import {
+  Environment,
+  Network,
+  Store,
+  RecordSource,
+  FetchFunction,
+} from "relay-runtime";
 
 const HTTP_ENDPOINT = "/api/graphql";
 
 const fetchGraphQL: FetchFunction = async (request, variables) => {
-    const resp = await fetch(HTTP_ENDPOINT, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ query: request.text, variables }),
-    });
-    if (!resp.ok) {
-        throw new Error("Response failed.");
-    }
-    return await resp.json();
+  const resp = await fetch(HTTP_ENDPOINT, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ query: request.text, variables }),
+  });
+  if (!resp.ok) {
+    throw new Error("Response failed.");
+  }
+  return await resp.json();
 };
 
 const environment = new Environment({
-    network: Network.create(fetchGraphQL),
-      store: new Store(new RecordSource()),
+  network: Network.create(fetchGraphQL),
+  store: new Store(new RecordSource()),
 });
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <RelayEnvironmentProvider environment={environment}>
-            <Suspense fallback="Loading...">
-                <App />
-            </Suspense>
-        </RelayEnvironmentProvider>
-    </StrictMode>,
+  <StrictMode>
+    <RelayEnvironmentProvider environment={environment}>
+      <Suspense fallback="Loading...">
+        <App />
+      </Suspense>
+    </RelayEnvironmentProvider>
+  </StrictMode>,
 );
